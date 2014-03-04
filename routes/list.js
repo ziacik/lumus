4fd78d77dff78e0eaee14cb93b48c7235cfdfb75
František Žiačik <ziacik@gmail.com>
2014-03-04 21:28:08 +0100

    Torrent Add, find best.

--- routes/list.js
+++ routes/list.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var transmissionSessionId;
+var torrentAddTries = 5;
 
 exports.list = function(req, res) {
 	res.render('list', {
@@ -7,43 +9,87 @@ exports.list = function(req, res) {
 	});
 };
 
+function fetchBestMovieResult($rootElements) {
+	if ($rootElements.length == 0)
+		return;
+	
+	doNext($rootElements, 0);
+}
+
+function doNext($rootElements, index) {
+	var $rootElement = $rootElements[index];
+
+	request('http://thepiratebay.se' + $($rootElement).children('.detLink').attr('href'), function(error, response, body) {
+		if (error) {
+			console.log(error);
+			return;
+		}
+				
+		if (body.indexOf('DTS') >= 0 || body.indexOf('AC3') >= 0 || body.indexOf('AC-3') >= 0) {
+			addTorrent($($rootElement).next().attr('href'));
+		} else {	
+			if (index < $rootElements.length) {
+				doNext($rootElements, index + 1);
+			}
+		}
+	});
+}
+
+function addTorrent(magnetLink) {	
+	var rpc = {};
+	rpc.arguments = {};
+	rpc.method = 'torrent-add';
+	rpc.arguments.filename = magnetLink;
+	
+	var options = {
+		url : 'http://malina:9091/transmission/rpc',
+		method : 'POST',
+		json : rpc,
+		headers : {
+			'X-Transmission-Session-Id' : transmissionSessionId
+		}
+	};
+
+	request(options, function(error, response, body) {
+		if (error) {
+			console.log(error);
+			tryAgainOrFail(function() { addTorrent(magnetLink); }, "Too many tries, getting error, giving up.");
+			return;
+		}
+		
+		if (response.statusCode == 409) {
+			transmissionSessionId = response.headers['x-transmission-session-id'];
+			tryAgainOrFail(function() { addTorrent(magnetLink); }, "Too many tries, getting 409, giving up.");
+			return;
+		}			
+		
+		console.log(body);
+	});
+}
+
+function tryAgainOrFail(doWhat, message) {
+	torrentAddTries--;
+	
+	if (torrentAddTries >= 0) {
+		doWhat();
+	} else {
+		console.log(message);
+		return;
+	}
+}
+
 exports.add = function(req, res) {
 	for (var key in req.body) {
 		console.log(key);
 		var url = "http://thepiratebay.se/search/" + key + "/0/7/207";
 
-		request(
-				url,
-				function(err, resp, body) {
-					if (err)
-						throw err;
-
-					$ = cheerio.load(body);
-
-					var magnetLink = $(".detName").first().next().attr('href');
-					var rpc = {};
-					rpc.arguments = {};
-					rpc.method = 'torrent-add';
-					rpc.arguments.filename = magnetLink;
-					
-					console.log(rpc);
-
-					var options = {
-							url : 'http://malina:9091/transmission/rpc',
-							method : 'POST',
-							json : rpc,
-							headers : {
-								'X-Transmission-Session-Id' : 'sOk7mIovL4vkPAH8jvyNgJY63RH9oupTUqmSqrMubE5O8pkj'
-							}
-						};
-
-					request(options, function(error, response, body) {
-						console.log(error);
-						console.log(body);
-					});
-
-				}
-			);
+		request(url, function(err, resp, body) {
+			if (err)
+				throw err;
+	
+			$ = cheerio.load(body);					
+			fetchBestMovieResult($(".detName"));
+		});
 	}
 
 	res.redirect('/');
