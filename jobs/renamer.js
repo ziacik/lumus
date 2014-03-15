70dca38939a9c34498d945859461439a1613fbfb
František Žiačik <ziacik@gmail.com>
2014-03-15 20:00:49 +0100

    Async renamer, remove existing dir.

--- jobs/renamer.js
+++ jobs/renamer.js
@@ -2,6 +2,7 @@ var config = require('../config');
 var fs = require('fs');
 var path = require('path');
 var process = require('child_process');
+var rmdir = require('rimraf');
 
 var Item = require('../models/item').Item;
 var ItemTypes = require('../models/item').ItemTypes;
@@ -9,21 +10,43 @@ var ItemStates = require('../models/item').ItemStates;
 
 //TODO: moje lokalne transmission rpc vracia pri duplicate torrente aj id, malina nie
 
+function doRename(item, destinationDir) {
+	fs.rename(item.downloadDir, destinationDir, function(error) {
+		if (error) {
+			console.log(error);
+			item.state = ItemStates.renameFailed;
+			item.save(function(err) {}); //TODO: err handling
+			return;	
+		}
+		
+		item.renamedDir = destinationDir;
+		item.state = ItemStates.renamed;
+		item.planNextCheck(1); //TODO hardcoded			
+		item.save(function(err) {}); //TODO: err handling
+	});
+}
+
 function rename(item) {
 	console.log("Renaming " + item.name);
 	
-	try {
-		var destinationDir = path.join(config[item.type + 'TargetDir'], item.name);	
-		fs.renameSync(item.downloadDir, destinationDir);
-		item.renamedDir = destinationDir;
-		item.state = ItemStates.renamed;
-		item.planNextCheck(1); //TODO hardcoded	
-	} catch (e) {
-		item.state = ItemStates.renameFailed;
-		console.log(e);
-	}
+	var destinationDir = path.join(config[item.type + 'TargetDir'], item.name);
 	
-	item.save(function(err) {}); //TODO: err handling	
+	fs.exists(destinationDir, function(exists) {
+		if (exists) {
+			rmdir(destionationDir, function(error) {
+				if (error) {
+					console.log(error);
+					item.state = ItemStates.renameFailed;
+					item.save(function(err) {}); //TODO: err handling
+					return;	
+				}
+				
+				doRename(item, destinationDir);			
+			});			
+		} else {
+			doRename(item, destinationDir);
+		}
+	});
 }
 
 module.exports.rename = rename;
