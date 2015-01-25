d285d94251b7c4ddbb4e68565c8242549fdad4bd
František Žiačik <ziacik@gmail.com>
2015-01-25 21:15:47 +0100

    Giving up client-side show search as most APIs don't support CORS. Shows are now searched on server using TVDB. Added error page. Using promises now for shows.

--- models/show.js
+++ models/show.js
@@ -1,4 +1,5 @@
 var Datastore = require('nedb');
+var Q = require('q');
 
 if (typeof db == 'undefined') 
 	db = {};
@@ -16,18 +17,29 @@ function Show() {
 }
 
 Show.setupMethods = function(show) {
-	show.save = function(done) {
-		if (!done)
-			throw "Sorry, done callback is required."; //TODO don't require callback
+	show.save = function() {
+		var deferred = Q.defer();	
 		
 		if (show._id) {
-			db.show.update({_id : show._id}, show, {}, done);
+			db.show.update({_id : show._id}, show, {}, function(err) {
+				if (err) {
+					deferred.reject(new Error(err));
+				} else {
+					deferred.resolve(show);
+				}
+			});
 		} else {
 			db.show.insert(show, function(err, newDoc) {
-				show._id = newDoc._id;
-				done(err);
+				if (err) {
+					deferred.reject(new Error(err));
+				} else {
+					show._id = newDoc._id;
+					deferred.resolve(show);
+				}
 			});
-		}		
+		}
+		
+		return deferred.promise;
 	};
 	
 	show.planNextCheck = function(seconds) {
@@ -51,13 +63,19 @@ Show.getAll = function(done) {
 	});
 };
 
-Show.findOne = function(what, done) {
-	db.show.findOne(what, function(err, show) {
-		if (show)
-			Show.setupMethods(show);
-		
-		done(err, show);
+Show.findOne = function(what) {
+	var deferred = Q.defer();	
+	db.show.findOne(what, function (err, show) {
+		if (err) {
+			deferred.reject(new Error(err));
+		} else {
+			if (show) {
+				Show.setupMethods(show)
+			}
+			deferred.resolve(show);
+		}
 	});
+	return deferred.promise;
 };
 
 Show.find = function(byWhat, done) {
@@ -76,8 +94,8 @@ Show.find = function(byWhat, done) {
 	});
 };
 
-Show.findById = function(id, done) {
-	Show.findOne({_id : id}, done);
+Show.findById = function(id) {
+	return Show.findOne({_id : id});
 };
 
 Show.removeById = function(id, done) {
--- routes/show.js
+++ routes/show.js
@@ -1,36 +1,106 @@
+var Q = require('q');
 var Show = require('../models/show').Show;
+var TvdbClient = require("node-tvdb");
+var tvdb = new TvdbClient("6E61D6699D0B1CB0");
 
 exports.add = function(req, res) {
-	Show.findOne({ showId : req.query.showId }, function(err, show) {
+	Show.findOne({ showId : req.query.showId })
+	.then(function(show) {
 		if (!show) {	
 			show = new Show();
 			show.name = req.query.name;
-			show.showId = req.query.showId;
-			
-			show.save(function(err) {
-				if (err) {
-					res.redirect('/error');	//TODO zle, loop
-					return;
-				}
-				
-				res.redirect('/show?id=' + show._id + '&showId=' + show.showId + '&name=' + encodeURIComponent(show.name));	
-			});
+			show.showId = req.query.showId;			
+			return show.save();
 		} else {
-			res.redirect('/show?id=' + show._id + '&showId=' + show.showId + '&name=' + encodeURIComponent(show.name));	
+			return Q(show);
 		}
-	});	
+	}).then(function(show) {
+		res.redirect('/show?id=' + show._id + '&showId=' + show.showId + '&name=' + encodeURIComponent(show.name));		
+	})
+	.catch(function(error) {
+		res.render('error', {
+			error: error
+		});		
+	});
 };
 
 exports.show = function(req, res) {
-	Show.findById(req.query.id, function(err, item) {
-		if (err) {
-			res.render('error', {
-				error: err
-			});
-		} else {
-			res.render('show', {
-				item: item
-			});		
-		}
+	var i;
+	
+	Show.findById(req.query.id)
+	.then(function(item) {		
+		i = item;
+		return getShowInfo(item);
+	})
+	.then(function(info) {
+		reduceShowInfo(info);
+		res.render('show', {
+			item: i,
+			info: info
+		});		
+	})
+	.catch(function(error) {
+		res.render('error', {
+			error: error
+		});		
 	});
 };
+
+var getShowInfo = function(item) {
+	console.log('Looking for item ' + item.showId + ' in tvdb.')	
+	return tvdb.getSeriesByRemoteId(item.showId).then(function (info) {
+		if (!info) {
+			throw 'Could not find season info about this show.'; 
+		}
+		return tvdb.getSeriesAllById(info.seriesid);
+	});
+}
+
+var reduceShowInfo = function(fullInfo) {
+	if (fullInfo[0] && fullInfo[0].SeriesName) {
+		fullInfo = fullInfo[0];
+	}
+	
+	var seasonInfo = fullInfo.Episodes.reduce(function(info, episode, index, array) {
+		if (episode.SeasonNumber !== '0') {
+			var seasonItem = findByNo(info, episode.SeasonNumber);
+			var episodeYear = getYear(episode.FirstAired);
+			
+			if (!seasonItem) {
+				seasonItem = {
+					No : episode.SeasonNumber,
+					Year : episodeYear
+				};
+				
+				info.push(seasonItem);
+			} else {
+				if (seasonItem.Year > episodeYear) {
+					seasonItem.Year = episodeYear;
+				}
+			}
+		}
+		
+		return info;
+	}, []);
+
+	fullInfo.seasons = seasonInfo;	
+	delete fullInfo.Episodes;
+}
+
+var findByNo = function(data, no) {
+	for (var i = 0; i < data.length; i++) {	
+		if (data[i].No == no) {
+			return data[i];
+		}
+	}
+	
+	return null;
+}
+
+var getYear = function(date) {
+	if (!date) {
+		return 'unknown';
+	} else {
+		return date.substr(0, date.indexOf('-'));
+	}
+}
