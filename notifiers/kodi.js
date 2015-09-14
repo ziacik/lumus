0c5289bf955e2c6282bd16eea9e90e5e9b9b0b3e
František Žiačik <ziacik@gmail.com>
2015-09-14 19:42:08 +0200

    Kodi-ws library used to update kodi. EventServer is now used for that.

--- notifiers/kodi.js
+++ notifiers/kodi.js
@@ -1,31 +1,53 @@
-var Q = require('q');
-var request = Q.denodeify(require('request'))
+var kodi = require('kodi-ws');
 var config = require('../config');
 var labels = require('../labels');
 
 var Item = require('../models/item').Item;
 var ItemTypes = require('../models/item').ItemTypes;
 
-config.add('kodi', { type : 'literal', store : {'notifier:kodi:use' : true, 'notifier:kodi:url':'http://localhost:8080'}});
-labels.add({ kodi : 'Kodi', 'notifier:kodi:url' : 'Kodi Url' });
+config.add('kodi', { type : 'literal', store : {'notifier:kodi:use' : true, 'notifier:kodi:host' : 'localhost', 'notifier:kodi:port' : '9090' }});
+labels.add({ 
+	kodi : 'Kodi', 
+	'notifier:kodi:url' : 'Kodi Url (Deprecated, please use Host and Port to EventServer)',
+	'notifier:kodi:host' : 'Kodi Host',
+	'notifier:kodi:port' : 'Kodi Port (EventServer)'
+});
 
 module.exports.notifySnatched = function(item) {
-	return doRpc('{"jsonrpc":"2.0", "method":"GUI.ShowNotification", "params":{"title":"Snatched", "message":"' + item.name + '","image":"info"}}');
+	return doStuff(function(connection) {
+		return connection.GUI.ShowNotification('Snatched', item.name, 'info');
+	});
 }
 
 module.exports.notifyDownloaded = function(item) {
-	return doRpc('{"jsonrpc":"2.0", "method":"GUI.ShowNotification", "params":{"title":"Downloaded", "message":"' + item.name + '","image":"info"}}');
+	return doStuff(function(connection) {
+		return connection.GUI.ShowNotification('Downloaded', item.name, 'info');
+	});
 }
 
 module.exports.updateLibrary = function(item) {
 	if (item.type === ItemTypes.music) {
-		return doRpc('{"jsonrpc":"2.0", "method":"AudioLibrary.Scan"}');
+		return updateAudioLibrary(item);
 	} else {
-		return doRpc('{"jsonrpc":"2.0", "method":"VideoLibrary.Scan"}');
+		return updateVideoLibrary(item);
 	}
 }
 
-function doRpc(payLoad) {
-	var url = config.get().notifier.kodi.url + "/jsonrpc?request=" + encodeURIComponent(payLoad);
-	return request(url);
+var updateAudioLibrary = function(item) {
+	return doStuff(function(connection) {
+		return connection.AudioLibrary.Scan();
+	});
+};
+
+var updateVideoLibrary = function(item) {
+	return doStuff(function(connection) {
+		return connection.VideoLibrary.Scan();
+	});
+};
+
+function doStuff(what) {
+	var options = config.get().notifier.kodi;	
+	return kodi(options.host, options.port).then(function(connection) {
+		return what(connection);
+	});
 }
