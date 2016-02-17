b5112cbe0f6fff11f080db13ec051badab3daaa8
František Žiačik <ziacik@gmail.com>
2016-02-17 16:32:10 +0100

    Homepage

--- api/controllers/ItemController.js
+++ api/controllers/ItemController.js
@@ -6,6 +6,43 @@
  */
 
 module.exports = {
-	
+	index : function(req, res) {
+		Item.find().then(function(items) {
+			res.view('homepage', {
+				items : items,
+				hasWaiting : function() {
+					return items.some(function(item) {
+						return item.isWaiting();
+					});
+				},
+			
+				hasDownloading : function() {
+					return items.some(function(item) {
+						return item.isDownloading();
+					});
+				},
+				
+				hasWaitingForSubtitles : function() {
+					return items.some(function(item) {
+						return item.isWaitingForSubtitles();
+					});
+				},
+				
+				hasFinished : function() {
+					return items.some(function(item) {
+						return item.isFinished();
+					});
+				},
+			
+				hasFailed : function() {
+					return items.some(function(item) {
+						return item.isFailed();
+					});
+				}			
+			});
+		}).fail(function(e) {
+			res.negotiate(e);
+		}).done();
+	}	
 };
 
